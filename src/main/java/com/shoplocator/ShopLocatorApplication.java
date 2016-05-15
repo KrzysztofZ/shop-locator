f63027c7cd68175c3412056eb9995b5da62d9c86
Krzysztof Zielinski <krzysztoffzielinski@gmail.com>
2016-05-15 01:43:18 +0100

    add app skeleton

---
+++ src/main/java/com/shoplocator/ShopLocatorApplication.java
@@ -0,0 +1,12 @@
+package com.shoplocator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopLocatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopLocatorApplication.class, args);
+	}
+}
